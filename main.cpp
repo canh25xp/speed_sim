9320039d318a2699d781433b7658be1ec62e9031
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-23 15:11:51 +0700

    add cli parser

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
-#include <iostream>
 #include <CLI/CLI.hpp>
+#include <iostream>
 
 using namespace std;
 
+#define GET(...) add_option(__VA_ARGS__)
+#define HAS(...) add_flag(__VA_ARGS__)
+
+int num_sensors = 0;
+int sampling = 0;
+int interval = 0;
+
 int main(int argc, char **argv) {
-  for (int i=1 ; i<argc; i++){
-    cout << argv[i] << endl;
-  }
+  CLI::App parser{"Speed sensor simulation and data analaysis"};
+  argv = parser.ensure_utf8(argv);
+
+  parser.GET("-n", num_sensors, "model file path");
+  parser.GET("-st", sampling, "file or folder or 0(webcam)");
+  parser.GET("-si", interval, "data file path");
+
+  CLI11_PARSE(parser, argc, argv);
+
+  cout << num_sensors << endl;
+  cout << sampling << endl;
+  cout << interval << endl;
+
   return 0;
 }
