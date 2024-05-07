d2805bbb2110e0a7be34c627bda598fdd29a3389
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-05-07 12:11:20 +0700

    add parser

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@ int sampling = 0;
 int interval = 0;
 
 int main(int argc, char** argv) {
+    Parser parser(argc, argv);
+    num_sensors = parser.get("-n", 0);
+    sampling = parser.get("-st", 0);
+    interval = parser.get("-si", 0);
     cout << num_sensors << endl;
     cout << sampling << endl;
     cout << interval << endl;
-
     return 0;
 }
