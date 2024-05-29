b1d90875e08cd8eb24d49b37828b86d1bd6b4bb5
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-05-29 14:15:46 +0700

    task template

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -1,46 +1,11 @@
-#include "csv.hpp"
-#include "parser.hpp"
-#include "simulation.hpp"
-
 #include <iostream>
-
-#define SAVE_NAME "speed_data.csv"
-
-int num_sensors = 0;
-int sampling = 0;
-int interval = 0;
 
 int
 main (int argc, char **argv)
 {
-  Parser parser (argc, argv);
-
-  num_sensors = parser.get ("-n", 1); // 1 sensor
-  sampling = parser.get ("-st", 10);  // 10 seconds
-  interval = parser.get ("-si", 1);   // 1 hour
-
-  std::cout << parser.getArg ();
-  std::cout << "Simulation started..." << std::endl;
-
-  // Create CSVHandler instance
-  CSVHandler csvHandler (SAVE_NAME);
-
-  if (!csvHandler.openFile ())
+  for (int i = 1; i < argc; i++)
     {
-      return 1;
+      std::cout << argv[i] << std::endl;
     }
-
-  // Write header to CSV file
-  csvHandler.writeHeader ({ "id", "time", "value" });
-
-  // The interval should be convert to seconds before passing to the
-  // Simulation: interval*60*60 However to save time, we will keep it as it.
-  runSimulation (num_sensors, sampling, interval, csvHandler);
-
-  // Close the file
-  csvHandler.closeFile ();
-
-  std::cout << "Simulation completed. Data saved to " << SAVE_NAME
-            << std::endl;
   return 0;
 }
