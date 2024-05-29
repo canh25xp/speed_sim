bd5d0765bba13b55875fc3714d00279100e36cac
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-05-29 22:48:41 +0700

    add process.cpp

---
+++ task_2/process.hpp
@@ -0,0 +1,15 @@
+#include <ctime>
+#include <string>
+
+const int LOWER_BOUND = 850;
+const int UPPER_BOUND = 1650;
+
+struct SpeedData;
+
+struct SummaryData;
+
+std::tm parseTime (const std::string &timeStr);
+
+std::string formatTime (const std::tm &tm);
+
+void processFile (const std::string &filename);
