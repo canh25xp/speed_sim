3844fa093b1f2b69e893805c1f37b08eca4cfefa
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-05-31 15:00:11 +0700

    refractor parser

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -1,13 +1,18 @@
+#include "parser.hpp"
 #include "process.hpp"
 #include <string>
 
 int
 main (int argc, char *argv[])
 {
+
+  Parser parser (argc, argv);
+
   std::string filename = "speed_data.csv";
-  if (argc > 1)
+
+  for (int i = 0; i < parser.getPositionalArgCount (); ++i)
     {
-      filename = argv[1];
+      filename = parser.getPositional (i);
     }
 
   processFile (filename);
