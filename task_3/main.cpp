1a6d0a5d82c45191d76b4ef554212930222c96ae
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-06-01 01:27:01 +0700

    add task 3 template

--- task_3/main.cpp
+++ task_3/main.cpp
@@ -1,8 +1,14 @@
+#include "common.hpp"
+#include "parser.hpp"
 #include <iostream>
 
 int
 main (int argc, char **argv)
 {
-  std::cout << "Hello Rachel";
+  Parser parser (argc, argv);
+
+  std::string inputFileName = parser.get (0, DATA_FILE_NAME);
+  std::string outputFileName = parser.get (1);
+  std::cout << inputFileName << " " << outputFileName << std::endl;
   return 0;
 }
